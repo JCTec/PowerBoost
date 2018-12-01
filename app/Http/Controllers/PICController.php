378fe2b11115900a27a48fd244e883cf956f59ce
Juan Carlos Estevez <jc_estevezr@hotmail.com>
2018-12-01 16:19:43 -0600

    Beta fix 2

--- app/Http/Controllers/PICController.php
+++ app/Http/Controllers/PICController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
-use Thujohn\Twitter\Twitter;
+use Thujohn\Twitter\Facades\Twitter;
 
 class PICController extends Controller
 {
